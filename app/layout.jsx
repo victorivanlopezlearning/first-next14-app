10ac109497e98ba144b716ac4f4193e3a2e2bef0
Víctor Iván López González <victor_ivan2211@hotmail.com>
2023-12-07 14:29:20 -0600

    Installed and Configured: Tailwind CSS

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,10 +1,4 @@
-import { Poppins } from 'next/font/google'
 import './globals.css'
-
-const poppins = Poppins({
-  weight: ['300', '400', '700'],
-  subsets: ['latin'],
-});
 
 export const metadata = {
   title: 'Next 14 App',
@@ -14,7 +8,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={poppins.className}>{children}</body>
+      <body>{children}</body>
     </html>
   )
 }
